578214a4d88941db89a3e8fb739ea37e4995726a
Garry Jones <garry.jones@mq.edu.au>
2018-11-29 13:19:20 +1100

    to use select

--- Post lunch session D2.R
+++ Post lunch session D2.R
@@ -1,1 +1,13 @@
 #### dplr and tidyr ####
+
+#import data ###
+
+library(tidyverse)
+
+interviews <- read_csv("data/SAFI_clean.csv", na="NULL")
+
+#Data frame with three columns ##
+select(interviews, village,  no_membrs, years_liv)
+
+select(interviews, village:rooms)
+
