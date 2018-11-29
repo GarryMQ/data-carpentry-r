37989842ae9a2f3151af5a9b2af1fcbea887ac6c
Garry Jones <garry.jones@mq.edu.au>
2018-11-29 13:29:54 +1100

    introduction to filtering

--- Post lunch session D2.R
+++ Post lunch session D2.R
@@ -11,3 +11,9 @@ select(interviews, village,  no_membrs, years_liv)
 
 select(interviews, village:rooms)
 
+####filtering is where you select rows #####
+#give me all the information on village that relates to the village of 'God' ##
+
+filter(interviews, village == "God")
+
+filter(interviews, village == "God", rooms > 2)
