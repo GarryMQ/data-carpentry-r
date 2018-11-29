22ba7299a5eae0f96d358834586ebb5c0bb38dcb
Garry Jones <garry.jones@mq.edu.au>
2018-11-29 14:41:27 +1100

    summarise multiple columns

--- Post lunch session D2.R
+++ Post lunch session D2.R
@@ -48,9 +48,67 @@ interviews %>% filter(memb_assoc == "yes") %>%
 # Want to add some new variable that is derived from others - we may want to know the average number of occupants per room - data continas how many rooms and household members, but is not expressed the way that we want - therefore mutaute - which allows us to add new columns ##
 
 ########### Mutate #####
+# this has added new column to the interviews data set ####
 
 interviews <- interviews %>% mutate(people_per_room = no_membrs/rooms)
 
 
+# want the average number per household
+# does not tell you if there are differences between villages ##
+mean(interviews$no_membrs)
+
+#so, there needds to be group_by ###
+# varaible that distonguished between differnet groups
+# shows as grouped by village
+
+interviews %>% group_by(village)
+
+# take grup data and pass through summarize
+
+interviews %>%
+  group_by(village) %>%
+  summarize(mean_no_membrs = mean(no_membrs))
+
+interviews %>%
+  filter(!is.na(memb_assoc)) %>%
+  group_by(village, memb_assoc) %>%
+  summarize(mean_no_membrs = mean(no_membrs))
+
+interviews %>%
+  filter(!is.na(memb_assoc)) %>%
+  group_by(village, memb_assoc) %>%
+  summarize(mean_no_membrs = mean(no_membrs),
+            min_membrs = min(no_membrs))
+
+##### conunt how many entries - count how often you see different values for village #####
+
+interviews %>%
+  count(village)     
+
+#if you want the count to be from largest to smallest ###
+interviews %>% count(village, sort = TRUE)
 
 
+###################### EXERCISE ##################
+
+interviews %>%
+  group_by(village) %>%
+  summarize(mean_no_membrs = mean(no_membrs))
+
+interviews %>%
+  group_by(village) %>%
+  summarize(mean_no_membrs = min(no_membrs))
+
+interviews %>%
+  group_by(village) %>%
+  summarize(mean_no_membrs = max(no_membrs))
+
+## in summary you can add n ####
+
+interviews %>% group_by(village) %>%
+  summarise (mean_membrs = mean (no_membrs),
+            min_membrs=min(no_membrs),
+            max_membrs = max(no_membrs),
+            n=n())
+
+            
