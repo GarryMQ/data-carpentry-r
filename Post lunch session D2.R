510a47301d0fdc516f2e12e18a17605ec472cb1b
Garry Jones <garry.jones@mq.edu.au>
2018-11-29 13:52:16 +1100

    Pipes

--- Post lunch session D2.R
+++ Post lunch session D2.R
@@ -17,3 +17,20 @@ select(interviews, village:rooms)
 filter(interviews, village == "God")
 
 filter(interviews, village == "God", rooms > 2)
+
+###### create intermeduate data frame #####
+interviews2 <-filter (interviews, village == "God")
+
+#selection for the columns and store in another data frame ### - with interviews 2 as the input ###
+interviews_god <- select(interviews2, no_membrs, years_liv)
+
+interviews_god <- select(filter(interviews, village == "God"),
+                         no_membrs, years_liv)
+
+## pipe is to fllow the arrow - first is the input data set - line break needs to be after the pipe symbol - does the filtering and selecting all in one go - it is cleaner than nesting the code ##
+
+interviews %>% filter(village == "God") %>%
+  select(no_membrs, years_liv)
+
+interviews_Chirodzo2 <- interviews %>% filter(village == "Chirodzo") %>% select(no_membrs, years_liv)
+
